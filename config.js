0ff34f271526268de08a1ac6226c7639f200061f
aiswaryawalter <aiswaryawalter@gmail.com>
2022-01-16 20:15:35 +0530

    update zora details

--- config.js
+++ config.js
@@ -1,2 +1,6 @@
-export const pranaAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-export const pranaHelperAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+
+  export const pranaAddress = "0x8d3D13D7289DA0e4d0cbaff6AF96E5F8dD157B95" //Mumbai
+  // export const pranaAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3" //local
+  export const pranaHelperAddress = "0x96117739Bb6Ba55d24D924a892108BAfd8B17377" //mumbai
+  export const auctionHouseMumbaiAddress = "0x6953190AAfD8f8995e8f47e8F014d0dB83E92300"
+  
