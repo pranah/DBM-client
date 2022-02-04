fa2be206aafbea42e6f3e7b10f5663ee7f4183af
arjunv <arjunvnd@gmail.com>
2022-02-04 23:20:41 +0530

    wip: save to token

--- components/high-light-drawer/HighLightDrawer.tsx
+++ components/high-light-drawer/HighLightDrawer.tsx
@@ -3,16 +3,24 @@ import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import { styled } from "@mui/material/styles";
 
+import { useRouter } from "next/router";
+import { useMoralisFile, useWeb3ExecuteFunction } from "react-moralis";
+
 import List from "@mui/material/List";
 import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import IconButton from "@mui/material/IconButton";
+import Button from "@mui/material/Button";
 import ListItemButton from "@mui/material/ListItemButton";
 import Typography from "@mui/material/Typography";
 import DeleteIcon from "@mui/icons-material/Delete";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import DrawerListItem from "../DrawerListItem/DrawerListItem";
+
+import { pranaAddress } from "../../config";
+import Prana from "../../artifacts/contracts/prana.sol/prana.json";
+
 const DrawerHeader = styled("div")(({ theme }) => ({
   display: "flex",
   alignItems: "center",
@@ -30,6 +38,51 @@ export default function HighLightDrawer({
   handleDrawerClose,
   handleSaveButtonClick,
 }) {
+  let router = useRouter();
+  const { url, tokenId } = router.query;
+  const { saveFile, isUploading } = useMoralisFile();
+  const contractProcessor = useWeb3ExecuteFunction();
+
+  const saveAnnotationToChain = async () => {
+    const metaDataJsonAnnotation = JSON.stringify(highLightText);
+    console.log("metaDataJsonAnnotation", metaDataJsonAnnotation);
+    const annotationIpfsIResp = await saveFile(
+      "annotation.json",
+      {
+        base64: window.btoa(
+          unescape(encodeURIComponent(metaDataJsonAnnotation))
+        ),
+      },
+      { saveIPFS: true }
+    );
+
+    const ipfsCID = annotationIpfsIResp?._ipfs;
+    let options = {
+      contractAddress: pranaAddress,
+      functionName: "setTokenURI",
+      abi: Prana.abi.filter((fn) => fn.name === "setTokenURI"),
+      params: {
+        _uniqueCIDfortoken: ipfsCID,
+        tokenId,
+      },
+    };
+    console.log("options", options);
+    try {
+      await contractProcessor.fetch({
+        params: options,
+        onError: (err) => {
+          console.log(err);
+          throw err;
+        },
+        onSuccess: () => {
+          console.log("success");
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <React.Fragment>
@@ -49,6 +102,10 @@ export default function HighLightDrawer({
               <Typography sx={{ px: 2 }} variant="h6" component="h6">
                 Highlights
               </Typography>
+
+              <Button onClick={saveAnnotationToChain}>
+                Save annotation to chain
+              </Button>
               <IconButton onClick={handleDrawerClose}>
                 <ChevronRightIcon />
               </IconButton>
