6f5cdee3fe5021e15bd1b9fdbf76ebea5026c627
arjunv <arjunvnd@gmail.com>
2022-02-09 23:06:54 +0530

    updated the margin

--- components/RentMyBook.tsx
+++ components/RentMyBook.tsx
@@ -69,12 +69,7 @@ export function RentMyBook({ bookName, tokenId, loadNFTs }) {
   return (
     <div>
       {isLoading && <Loader />}
-      <Button
-        size="large"
-        sx={{ ml: 1 }}
-        variant="outlined"
-        onClick={handleClickOpen}
-      >
+      <Button size="large" variant="outlined" onClick={handleClickOpen}>
         Rent{" "}
       </Button>
       <Dialog
